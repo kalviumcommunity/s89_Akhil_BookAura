fa34c5825d03c385e115d3f3e9251bf65e595238
AkhilRaj1819 <vysyaraju.akhilraju.s89@kalvium.community>
2025-06-07 16:20:39 +0530

    epubbb

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import './components/GoogleTranslateGlobal.css'
-import './utils/hideGoogleTranslate.js'
 import { BrowserRouter } from 'react-router-dom'
 import AllRouting from './AllRouting'
 import { CartProvider } from './pages/MarketPlace/cart'
